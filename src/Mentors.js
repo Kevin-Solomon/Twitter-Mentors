9f8e6e09b2c703dfb4ee6ce91210e12030ded34a
Hamdrive <hamza.husein017@gmail.com>
2021-12-03 23:49:00 +0530

    [feat] call serverless function in App

--- src/Mentors.js
+++ src/Mentors.js
@@ -1,32 +1,13 @@
 import React from "react";
+import axios from "axios";
 
-function fetchMentor() {
-  var myHeaders = new Headers();
-  myHeaders.append(
-    "Authorization",
-    "Bearer AAAAAAAAAAAAAAAAAAAAACjtWAEAAAAAa1FwPeaT5c2jgcKtxTf%2BaBCDxac%3DGV1ybVBbMfxT8BkG9aDY0V4iTOYcoT3MpUsTvf37Of7dyhtjo0"
-  );
-  myHeaders.append(
-    "Cookie",
-    'guest_id=v1%3A163798981035928505; guest_id_ads=v1%3A163798981035928505; guest_id_marketing=v1%3A163798981035928505; personalization_id="v1_j0aP1wbL37FX/9BsALm1hA=="'
-  );
-
-  var requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-  };
-
-  fetch(
-    "https://api.twitter.com/2/users/by/username/tanaypratap?user.fields=profile_image_url,name",
-    requestOptions
-  )
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-}
 
 function Mentors() {
+  async function fetchMentor(){
+    const result = await axios.get('./netlify/functions/mentorFunction')
+    console.log(result)
+  }
+
   return (
     <div>
       <button onClick={fetchMentor}>Get Mentor</button>
