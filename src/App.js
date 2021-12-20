8da82ac3984e1f70934128ffd10d13f326fd1161
Hamdrive <hamza.husein017@gmail.com>
2021-12-20 19:36:48 +0530

    [style] touchup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AllMentors from "./AllMentors";
 function App() {
   return (
     <div className="App">
-      <h2 className="title">Twitter Mentors</h2>
+      <p className="title">Twitter Mentors</p>
       <AllMentors />
     </div>
   );
